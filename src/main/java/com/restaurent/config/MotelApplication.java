c086b81a6ae37e68364521de0b6048281466b10b
neruppuda <araghu.95>
2019-08-22 23:20:32 +0530

    Initial commit

--- src/main/java/com/restaurent/config/MotelApplication.java
+++ src/main/java/com/restaurent/config/MotelApplication.java
@@ -10,9 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages= {"com.restaurent.controller","com.restaurent.service.impl"})
 @EnableJpaRepositories(basePackages= {"com.restaurent.dao"})
 @EntityScan(basePackages= {"com.restaurent.model"})
-
 public class MotelApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MotelApplication.class, args);
 	}
---
+++ src/main/java/com/restaurent/controller/SingletonClass.java
@@ -0,0 +1,4 @@
+package com.restaurent.controller;
+
+public class SingletonClass {
+}
